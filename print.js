991a45248a306aa610d36e86aa97a20f662573f8
Fladimir97 <f.flad@hotmail.de>
2026-07-15 12:05:32 +0200

    Commit

--- print.js
+++
@@ -1,7 +0,0 @@
-function changeprintSheet(){
-	let stylesheet = document.getElementById("DruckOpt").value;
-	let stylesheetTag = document.getElementById("printsheet");
-	stylesheetTag.setAttribute("href", `${stylesheet}`);
-	ausw();
-	
-	}
